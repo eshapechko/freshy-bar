90e45981f71ec0c8ac99c2c2a8185c69f6c02886
Evgeniy Shapechko <shapen86@mail.ru>
2023-08-02 19:19:42 +0300

    add modal order

--- js/script.js
+++ js/script.js
@@ -29,7 +29,47 @@ const createCard = (item) => {
     return cocktail;
 };
 
+const modalController = ({ modal, btnOpen, time = 300 }) => {
+    const buttonElem = document.querySelector(btnOpen);
+    const modalElem = document.querySelector(modal);
+    console.log("modalElem: ", modalElem);
+
+    modalElem.style.cssText = `
+    display: flex;
+    visibility: hidden;
+    opacity: 0;
+    transition: opacity ${time}ms ease-in-out;
+    `;
+
+    const closeModal = (event) => {
+        const target = event.target;
+        const code = event.code;
+
+        if (target === modalElem || code === "Escape") {
+            modalElem.style.opacity = 0;
+
+            setTimeout(() => {
+                modalElem.style.visibility = "hidden";
+            }, time);
+
+            window.removeEventListener("keydown", closeModal);
+        }
+    };
+
+    const openModal = () => {
+        modalElem.style.visibility = "visible";
+        modalElem.style.opacity = 1;
+        window.addEventListener("keydown", closeModal);
+    };
+
+    buttonElem.addEventListener("click", openModal);
+    modalElem.addEventListener("click", closeModal);
+
+    return { openModal, closeModal };
+};
+
 const init = async () => {
+    modalController({ modal: ".modal_order", btnOpen: ".header__btn-order" });
     const goodsListElem = document.querySelector(".goods__list");
     const data = await getData();
 
